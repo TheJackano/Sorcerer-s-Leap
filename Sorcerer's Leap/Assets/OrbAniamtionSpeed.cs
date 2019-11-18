09085e2ae7b097a2fbf67c1237863ea70aac48ad
GameBreaker74 <37140235+GameBreaker74@users.noreply.github.com>
2019-11-18 16:52:23 +0000

    Lights

---
+++ Sorcerer's Leap/Assets/OrbAniamtionSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbAniamtionSpeed : MonoBehaviour
+{
+    public float HighVal;
+    public float LowVal;
+
+    private Animator orbAnimator;
+    private float animSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbAnimator = GetComponent<Animator>();
+        animSpeed = Random.Range(HighVal, LowVal);
+        orbAnimator.speed = animSpeed;
+    }
+
+}
