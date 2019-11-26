1ea1c438f11d54e33e6ab848d202801f0c8a56d8
TheJackano <jessenunes29@gmail.com>
2019-11-26 13:40:08 +0000

    UI Elements and code set up to show the elements the player and AI have selected

--- Sorcerer's Leap/Assets/Scripts/GameController.cs
+++ Sorcerer's Leap/Assets/Scripts/GameController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     Enemy[] Enemies;
     string[] Element = {"Fire","Earth","Water","Metal","Wood"};
     public GameObject[] objEnemies;
+    public GameObject Player;
+    public Sprite[] imgElements;
 
 
 
@@ -16,6 +19,24 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
+        {
+            objEnemies[0].transform.Find("Selection").gameObject.SetActive(false);
+            objEnemies[1].transform.Find("Selection").gameObject.SetActive(false);
+            objEnemies[2].transform.Find("Selection").gameObject.SetActive(false);
+        }//Deselects Enemy Targets Visually
+        {
+            Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[5];
+            objEnemies[0].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[5];
+            objEnemies[1].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[5];
+            objEnemies[2].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[5];
+        }//Sets all the UI Elements Selections to ? image
+        strPhase = "Selection";
+
+        /*
+        Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[3];
+        objEnemies[0].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[1];
+        objEnemies[1].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[3];
+        */
         /*
         Debug.Log(Element.Length);
         for (int i = 0; i < Element.Length; i++)
@@ -29,14 +50,12 @@ public class GameController : MonoBehaviour
     {
         if (Input.inputString == "s") strPhase = "Selection";
         if (Input.inputString == "c") strPhase = "Combat";
-        //objEnemies[0].transform.GetChild(3).gameObject.SetActive(false);
 
         switch (strPhase)
         {
             case "Selection":
                 SelectedElement();
                 SelectedTargets();
-                ToggleSelectedTargetUI();
                 break;
             case "Combat":
                 //Debug.Log(strSelectedElement);
@@ -47,13 +66,6 @@ public class GameController : MonoBehaviour
         }
 
 
-        /*
-         * 1. Select Element
-         * 2. Select Target
-         * 
-         *
-         */
-
     }
     private void SelectedElement()
     {
@@ -61,67 +73,63 @@ public class GameController : MonoBehaviour
         {
             case "q":
                 strSelectedElement = Element[0];
+                Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[0];
                 break;
             case "w":
                 strSelectedElement = Element[1];
+                Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[1];
                 break;
             case "e":
                 strSelectedElement = Element[2];
+                Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[2];
                 break;
             case "r":
                 strSelectedElement = Element[3];
+                Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[3];
                 break;
             case "t":
                 strSelectedElement = Element[4];
+                Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[4];
                 break;
         }
         Debug.Log(strSelectedElement);
-    }
+    } //Allows the User to Select an Element
     private void SelectedTargets()
     {
         switch (Input.inputString)
         {
             case "4":
                 strSelectedTarget = "Left";
+                {
+                    objEnemies[0].transform.Find("Selection").gameObject.SetActive(true);
+                    objEnemies[1].transform.Find("Selection").gameObject.SetActive(false);
+                    objEnemies[2].transform.Find("Selection").gameObject.SetActive(false);
+                }//Displays Left Target Selection
+
                 break;
             case "5":
                 strSelectedTarget = "Middle";
+                {
+                    objEnemies[0].transform.Find("Selection").gameObject.SetActive(false);
+                    objEnemies[1].transform.Find("Selection").gameObject.SetActive(true);
+                    objEnemies[2].transform.Find("Selection").gameObject.SetActive(false);
+                }//Displays Middle Target Selection
                 break;
             case "6":
                 strSelectedTarget = "Right";
+                {
+                    objEnemies[0].transform.Find("Selection").gameObject.SetActive(false);
+                    objEnemies[1].transform.Find("Selection").gameObject.SetActive(false);
+                    objEnemies[2].transform.Find("Selection").gameObject.SetActive(true);
+                }//Displays Right Target Selection
                 break;
             case "2":
                 strSelectedTarget = "All";
+                //Displays All Target Selection
                 break;
         }
         Debug.Log(strSelectedTarget);
-    }
-    private void ToggleSelectedTargetUI()
-    {
-        switch (strSelectedTarget)
-        {
-            case "Left":
-                objEnemies[0].transform.Find("Selection").gameObject.SetActive(true);
-                objEnemies[1].transform.Find("Selection").gameObject.SetActive(false);
-                objEnemies[2].transform.Find("Selection").gameObject.SetActive(false);
-                break;
-            case "Middle":
-                objEnemies[0].transform.Find("Selection").gameObject.SetActive(false);
-                objEnemies[1].transform.Find("Selection").gameObject.SetActive(true);
-                objEnemies[2].transform.Find("Selection").gameObject.SetActive(false);
-                break;
-            case "Right":
-                objEnemies[0].transform.Find("Selection").gameObject.SetActive(false);
-                objEnemies[1].transform.Find("Selection").gameObject.SetActive(false);
-                objEnemies[2].transform.Find("Selection").gameObject.SetActive(true);
-                break;
-            case "All":
-                objEnemies[0].transform.Find("Selection").gameObject.SetActive(true);
-                objEnemies[1].transform.Find("Selection").gameObject.SetActive(true);
-                objEnemies[2].transform.Find("Selection").gameObject.SetActive(true);
-                break;
-        }
-    }
+    } //Allows the User to Select a Target
 }
 public class Enemy
 {
