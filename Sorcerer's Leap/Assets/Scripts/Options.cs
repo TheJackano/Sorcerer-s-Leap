0baf32486518fa0aa78caa6898533c8ba3a279d2
GameBreaker74 <37140235+GameBreaker74@users.noreply.github.com>
2019-11-26 11:42:32 +0000

    Ui Scripting

---
+++ Sorcerer's Leap/Assets/Scripts/Options.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OptionsOnClick()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+}
---
+++ Sorcerer's Leap/Assets/Scripts/Play.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject tutorial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayOnClick()
+    {
+        mainMenu.SetActive(false);
+        tutorial.SetActive(true);
+    }
+}
