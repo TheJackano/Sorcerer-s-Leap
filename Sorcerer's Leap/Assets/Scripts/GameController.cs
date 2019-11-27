0a6feab08659eb43c91bba63f29533cac057ab4f
TheJackano <jessenunes29@gmail.com>
2019-11-27 04:06:02 +0000

    AI now select a random ability

--- Sorcerer's Leap/Assets/Scripts/GameController.cs
+++ Sorcerer's Leap/Assets/Scripts/GameController.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    Enemy[] Enemies;
+    Enemy[] Enemies = new Enemy[]
+        {
+            new Enemy("Water"),
+            new Enemy("Earth"),
+            new Enemy("Fire"),
+        };
     string[] Element = {"Fire","Earth","Water","Metal","Wood"};
     public GameObject[] objEnemies;
     public GameObject Player;
     public Sprite[] imgElements;
 
-
-
     string strPhase = "";
     string strSelectedElement = "";
     string strSelectedTarget = "";
+
+    bool AIAssignedElement = false;
 
     void Start()
     {
@@ -30,42 +35,25 @@ public class GameController : MonoBehaviour
             objEnemies[1].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[5];
             objEnemies[2].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[5];
         }//Sets all the UI Elements Selections to ? image
-        strPhase = "Selection";
-
-        /*
-        Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[3];
-        objEnemies[0].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[1];
-        objEnemies[1].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[3];
-        */
-        /*
-        Debug.Log(Element.Length);
-        for (int i = 0; i < Element.Length; i++)
-        {
-            Debug.Log(Element[i]);
-        }
-        */
+        strPhase = "Selection";   
     }
 
     void Update()
     {
-        if (Input.inputString == "s") strPhase = "Selection";
-        if (Input.inputString == "c") strPhase = "Combat";
-
         switch (strPhase)
         {
             case "Selection":
                 SelectedElement();
                 SelectedTargets();
+                if (Input.GetKey(KeyCode.Space) && strSelectedElement != "" && strSelectedTarget !="") strPhase = "Combat";
                 break;
             case "Combat":
-                //Debug.Log(strSelectedElement);
-                //Debug.Log(strSelectedTarget);
+                if (!AIAssignedElement) RandomAIElement();
+                //CalculateDamage
                 break;
             case "Shopping":
                 break;
         }
-
-
     }
     private void SelectedElement()
     {
@@ -92,7 +80,6 @@ public class GameController : MonoBehaviour
                 Player.transform.Find("Element").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[4];
                 break;
         }
-        Debug.Log(strSelectedElement);
     } //Allows the User to Select an Element
     private void SelectedTargets()
     {
@@ -125,16 +112,59 @@ public class GameController : MonoBehaviour
                 break;
             case "2":
                 strSelectedTarget = "All";
-                //Displays All Target Selection
+                {
+                    objEnemies[0].transform.Find("Selection").gameObject.SetActive(true);
+                    objEnemies[1].transform.Find("Selection").gameObject.SetActive(true);
+                    objEnemies[2].transform.Find("Selection").gameObject.SetActive(true);
+                }//Displays All Target Selection
                 break;
         }
-        Debug.Log(strSelectedTarget);
     } //Allows the User to Select a Target
+    private void RandomAIElement()
+    {
+        for(int i = 0; i < objEnemies.Length; i++)
+        {
+            int RandomNumber = Random.Range(0,99);
+            switch (RandomNumber)//"Fire","Earth","Water","Metal","Wood"
+            {
+                case int n when n >=0 && n<=9://Fire
+                    Enemies[i].SelectedSpell = "Fire";
+                    break;
+                case int n when n >= 10 && n <= 19://Earth
+                    Enemies[i].SelectedSpell = "Earth";
+                    break;
+                case int n when n >= 20 && n <= 29://Water
+                    Enemies[i].SelectedSpell = "Water";
+                    break;
+                case int n when n >= 30 && n <= 39://Metal
+                    Enemies[i].SelectedSpell = "Metal";
+                    break;
+                case int n when n >= 40 && n <= 49://Wood
+                    Enemies[i].SelectedSpell = "Wood";
+                    break;
+                case int n when n >= 50 && n <= 99://Affinity
+                    Enemies[i].SelectedSpell = Enemies[i].WizzardType;
+                    Debug.Log(Enemies[i].WizzardType + "Special");
+                    break;
+            }
+            for(int k = 0; k < Element.Length; k++)
+            {
+                if (Enemies[i].SelectedSpell == Element[k]) objEnemies[i].transform.Find("Canvas").Find("Image").gameObject.GetComponent<Image>().overrideSprite = imgElements[k];
+            }
+            Debug.Log(Enemies[i].WizzardType + Enemies[i].SelectedSpell);
+        }
+        AIAssignedElement = true; 
+    } //Selectes a random element to each AI
 }
 public class Enemy
 {
-    int Health;
-    string WizzardType;
-    string SelectedSpell;
-    string position;
+    public int Health = 100;
+    public string WizzardType;
+    public string SelectedSpell;
+
+    public Enemy(string _WizzardType)
+    {
+        WizzardType = _WizzardType;
+    }
 }
+
