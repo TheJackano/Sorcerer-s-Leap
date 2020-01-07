181fca64a01c3e354451324f5df894ec8d222aa8
GameBreaker74 <37140235+GameBreaker74@users.noreply.github.com>
2020-01-07 10:53:46 +0000

    MainMenu

---
+++ Sorcerer's Leap/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject menu;
+    public GameObject options;
+    public GameObject playMarker;
+    public GameObject optionMarker;
+    public GameObject quitMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play() {
+        playMarker.SetActive(true);
+        SceneManager.LoadScene("TutorialCourtyard");
+    }
+
+    public void Quit() {
+        quitMarker.SetActive(true);
+        Application.Quit();
+    }
+
+    public void Options() {
+        optionMarker.SetActive(true);
+        menu.SetActive(false);
+        options.SetActive(true);
+        optionMarker.SetActive(false);
+    }
+
+    public void Back() {
+        options.SetActive(false);
+        menu.SetActive(true);
+    }
+}
