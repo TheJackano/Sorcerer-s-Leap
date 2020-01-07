4a61792cb642004f730be10b47bde3c15d87a178
Megan <mgibsonmandalore@gmail.com>
2020-01-07 18:09:37 +0000

    ArenaPause

--- Sorcerer's Leap/Assets/Scripts/ArenaController.cs
+++ Sorcerer's Leap/Assets/Scripts/ArenaController.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
+using UnityEngine.SceneManagement;
 
 public class ArenaController : MonoBehaviour
 {
 	public Transform shopCam = null;
 	public GameObject camera;
 	public Animator camAnimation;
+	public bool shopOpen;
+	public GameObject pauseMenu;
+	public bool pauseActive;
 
 	private KeywordRecognizer keywordRecognizer;
 	private Dictionary<string, Action> actions = new Dictionary<string, Action>();
@@ -19,6 +23,9 @@ public class ArenaController : MonoBehaviour
     {
 		actions.Add("Shop", Shop);
 		actions.Add("Checkout", Checkout);
+		actions.Add("Pause", Pause);
+		actions.Add("Resume", Resume);
+		actions.Add("Restart", Restart);
 
 		keywordRecognizer = new KeywordRecognizer (actions.Keys.ToArray());
 		keywordRecognizer.OnPhraseRecognized += VoiceInput;
@@ -39,12 +46,46 @@ public class ArenaController : MonoBehaviour
 	}
 
 	public void Shop() {
+		shopOpen = true;
 		camAnimation.Play("Shop");
 		
 }
 		
 	public void Checkout(){
+		if(shopOpen == true){
 		camAnimation.Play("BackToFight");
+			shopOpen = false;
+		}
+	}
+
+	public void Pause(){
+		pauseMenu.SetActive(true);
+		pauseActive = true;
+		Time.timeScale = 0;
+	}
+
+	public void Resume (){
+		if(pauseActive == true){
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1;
+			pauseActive = false;
+		}
+	}
+
+	public void Restart() {
+		if(pauseActive == true){
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1;
+			pauseActive = false;
+		SceneManager.LoadScene("MainArena");
+		}
+	}
+
+	void OnDestroy(){
+		if(keywordRecognizer != null){
+			keywordRecognizer.Stop();
+			keywordRecognizer.Dispose();
+		}
 	}
 
 }
