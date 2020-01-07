d1ca4295ee9b894acd6ee6f2358113d7c0d74f0e
Megan <mgibsonmandalore@gmail.com>
2020-01-07 17:43:53 +0000

    Camera moves

---
+++ Sorcerer's Leap/Assets/Scripts/ArenaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class ArenaController : MonoBehaviour
+{
+	public Transform shopCam = null;
+	public GameObject camera;
+	public Animator camAnimation;
+
+	private KeywordRecognizer keywordRecognizer;
+	private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		actions.Add("Shop", Shop);
+		actions.Add("Checkout", Checkout);
+
+		keywordRecognizer = new KeywordRecognizer (actions.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += VoiceInput;
+		keywordRecognizer.Start();
+
+		camAnimation = camera.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void VoiceInput(PhraseRecognizedEventArgs speech){
+		Debug.Log(speech.text);
+		actions[speech.text].Invoke();
+	}
+
+	public void Shop() {
+		camAnimation.Play("Shop");
+		
+}
+		
+	public void Checkout(){
+		camAnimation.Play("BackToFight");
+	}
+
+}
